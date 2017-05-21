8f468fc08c4db96309595af4fdbf7b7afaba9603
Jah <jah@jah.jah>
2017-05-22 10:27:28 +1100

    !update

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -11,7 +11,7 @@ public class Calculate {
 	* Точка входа.
 	* @param args аргументы приложения
 	*/
-	public static void main(String[] args) {
+	public static void main(String ... args) {
 		System.out.println("Hello World");
 	}
 }
