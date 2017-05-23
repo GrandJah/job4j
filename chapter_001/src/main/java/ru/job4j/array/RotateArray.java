975fa00febebc631d4464fe290b172acaeb1a9d4
Jah <jah@jah.jah>
2017-05-23 13:31:12 +1100

    update RotateArray

--- chapter_001/src/main/java/ru/job4j/array/RotateArray.java
+++ chapter_001/src/main/java/ru/job4j/array/RotateArray.java
@@ -8,19 +8,42 @@ package ru.job4j.array;
  * @since 22.05.2017
  */
 public class RotateArray {
-    /**
+    /**Поворачивает массив по часовой стрелке.
+     * создает новый массив, если исходный массив не квадратный
      * @param array двумерный массив
      * @return повернутый массив по часовой стрелке
      */
     public int[][] rotate(int[][] array) {
         int rows = array.length;
         int cols = array[0].length;
-        int[][] returnArray = new int[cols][rows];
-        for (int n = 0; n < cols; n++) {
-            for (int m = 0; m < rows; m++) {
-                returnArray[n][m] = array[rows - 1 - m][n];
+        int[][] returnArray;
+        if (rows == cols) {
+            returnArray = rotateSquareArray(array);
+        } else {
+            returnArray = new int[cols][rows];
+            for (int n = 0; n < cols; n++) {
+                for (int m = 0; m < rows; m++) {
+                    returnArray[n][m] = array[rows - 1 - m][n];
+                }
             }
         }
         return returnArray;
+    }
+    /** Поворачивает квадратный массив.
+     * @param array исходный массив
+     * @return повернутый массив
+     */
+    private int[][] rotateSquareArray(int[][] array) {
+        int maxIndex = array.length - 1;
+        for (int indexM = 0; indexM < array.length / 2 + array.length % 2; indexM++) {
+            for (int indexN = 0; indexN < array.length / 2; indexN++) {
+                int tmp = array[indexM][indexN];
+                array[indexM][indexN] = array[maxIndex - indexN][indexM];
+                array[maxIndex - indexN][indexM] = array[maxIndex - indexM][maxIndex - indexN];
+                array[maxIndex - indexM][maxIndex - indexN] = array[indexN][maxIndex - indexM];
+                array[indexN][maxIndex - indexM] = tmp;
+            }
+        }
+        return array;
     }
 }
