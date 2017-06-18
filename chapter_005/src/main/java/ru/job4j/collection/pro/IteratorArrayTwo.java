dec4e9522916a04564c8305cc081d4e60318c764
Jah <jah@jah.jah>
2017-06-18 21:00:49 +1100

    task 17356

---
+++ chapter_005/src/main/java/ru/job4j/collection/pro/IteratorArrayTwo.java
@@ -0,0 +1,45 @@
+package ru.job4j.collection.pro;
+
+import java.util.Iterator;
+
+/**
+ * junior.
+ *
+ * @author Igor Kovalkov
+ * @version 0.1
+ * @since 18.06.2017
+ */
+public class IteratorArrayTwo implements Iterator {
+    /**
+     * Массив.
+     */
+    private int[][] value;
+    /**
+     * Индексы.
+     */
+    private int indexN, indexM;
+
+    /**
+     * @param value Иассив
+     */
+    public IteratorArrayTwo(int[][] value) {
+        this.value = value;
+        this.indexM = 0;
+        this.indexN = 0;
+    }
+
+    @Override
+    public boolean hasNext() {
+        return this.indexN < this.value.length;
+    }
+
+    @Override
+    public Object next() {
+        int ret = this.value[this.indexN][this.indexM++];
+        if (this.indexM >= this.value[0].length) {
+            this.indexN++; this.indexM = 0;
+        }
+
+        return ret;
+    }
+}
---
+++ chapter_005/src/test/java/ru/job4j/collection/pro/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * junior.
+ *
+ * @author Igor Kovalkov
+ * @version 0.1
+ * @since 18.06.2017
+ */
+package ru.job4j.collection.pro;
