360d4b6c4ca6da28e11c2c4634557ed13fc689c9
Jah <jah@jah.jah>
2017-11-04 09:04:11 +1100

    Task 999

---
+++ chapter_005/src/main/java/ru/job4j/collection/pro/map/User.java
@@ -0,0 +1,26 @@
+package ru.job4j.collection.pro.map;
+
+import java.util.Calendar;
+
+/**
+ * junior.
+ *
+ * @author Igor Kovalkov
+ * @version 0.1
+ * @since 04.11.2017
+ */
+public class User {
+    /**
+     * Имя пользователя.
+     */
+    private String name;
+    /**
+     * Кол-во детей.
+     */
+    private int children;
+    /**
+     * День рождения.
+     */
+    private Calendar birthday;
+
+}
