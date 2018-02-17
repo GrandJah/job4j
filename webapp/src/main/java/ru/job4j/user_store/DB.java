5fd831dc9fced0b5e38919ce1669273aafcc45cb
Jah <jah@jah.jah>
2018-02-17 21:13:25 +1100

    fix DB

--- webapp/src/main/java/ru/job4j/user_store/DB.java
+++ webapp/src/main/java/ru/job4j/user_store/DB.java
@@ -2,12 +2,14 @@ package ru.job4j.user_store;
 
 import org.apache.commons.dbcp.DriverManagerConnectionFactory;
 import org.apache.commons.dbcp.PoolableConnectionFactory;
+//import org.apache.commons.dbcp.PoolingDriver;
 import org.apache.commons.dbcp.PoolingDataSource;
 import org.apache.commons.pool.impl.GenericObjectPool;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.sql.Connection;
+//import java.sql.DriverManager;
 import java.sql.ParameterMetaData;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
