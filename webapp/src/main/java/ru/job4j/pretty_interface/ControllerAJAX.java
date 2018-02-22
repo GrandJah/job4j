8f72f97d0bcfe1223921ae46d8e0368b10bae998
Jah <jah@jah.jah>
2018-02-22 11:34:58 +1100

    add new Servlet

---
+++ webapp/src/main/java/ru/job4j/pretty_interface/ControllerAJAX.java
@@ -0,0 +1,13 @@
+package ru.job4j.pretty_interface;
+
+import javax.servlet.http.HttpServlet;
+
+/**
+ * junior.
+ *
+ * @author Igor Kovalkov
+ * @version 0.1
+ * @since 19.02.2018
+ */
+public class ControllerAJAX extends HttpServlet {
+}
---
+++ webapp/src/main/java/ru/job4j/store/ILocationStore.java
@@ -0,0 +1,8 @@
+package ru.job4j.store;
+
+/**
+ * Location store interface.
+ */
+public class ILocationStore {
+
+}
