44e9c120e64c46a82cd7fa18ec5980c404e39bce
Jah <jah@jah.jah>
2020-12-04 23:19:16 +1100

    add Hibernate

--- level3/tracker/src/main/java/ru/job4j/tracker/Item.java
+++ level3/tracker/src/main/java/ru/job4j/tracker/Item.java
@@ -1,5 +1,11 @@
 package ru.job4j.tracker;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -12,10 +18,14 @@ import lombok.NoArgsConstructor;
  */
 @Data
 @NoArgsConstructor(force = true)
+@Entity
+@Table(name = "items")
 public class Item {
   /**
    * Id заявки.
    */
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Integer id;
 
   /**
