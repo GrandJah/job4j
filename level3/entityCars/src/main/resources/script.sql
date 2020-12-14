96588993004c5f661eda333d2744f8d674de3501
CrandJah <bujhtitxrf@gmail.com>
2020-12-15 07:15:18 +1100

    example relations table entityCars add SQL script

---
+++ level3/entityCars/src/main/resources/script.sql
@@ -0,0 +1,28 @@
+create table drivers
+(
+	id integer primary key,
+	name varchar(255)
+);
+
+create table engines
+(
+	id integer primary key,
+	model varchar(255),
+	serialnumber varchar(255)
+);
+
+create table cars
+(
+	id integer primary key,
+	serialnumber varchar(255),
+	engine_id references engines(id)
+);
+
+create table history_owner
+(
+	id integer primary key,
+	endOwner timestamp,
+	startOwner timestamp,
+	car_id integer not null references cars(id),
+	driver_id integer not null references drivers(id)
+);
