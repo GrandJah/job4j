20ad8d9831eb89f3c90d0ce690904e30b13fa3a8
CrandJah <bujhtitxrf@gmail.com>
2020-12-16 18:21:54 +1100

    add hibernate frame

---
+++ level3/sell_car/src/main/java/ru/job4j/sell_car/storage/HbmStorage.java
@@ -0,0 +1,55 @@
+package ru.job4j.sell_car.storage;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
+import java.util.function.Function;
+
+public abstract class HbmStorage {
+
+   private final SessionFactory sf = Connect.sf();
+
+   protected <T> T query(Function<Session, T> function) {
+      Session session = this.sf.openSession();
+      Transaction tx = session.beginTransaction();
+      try {
+         T result = function.apply(session);
+         tx.commit();
+         return result;
+      } catch (final Exception e) {
+         tx.rollback();
+         throw e;
+      } finally {
+         session.close();
+      }
+   }
+
+   static class Connect implements AutoCloseable {
+
+      private static final Connect CONNECT = new Connect();
+
+      private Connect() {
+         System.out.println("Вызван конструктор Connect");
+      }
+
+      public static SessionFactory sf() {
+         return CONNECT.sf;
+      }
+
+      private final StandardServiceRegistry registry =
+       new StandardServiceRegistryBuilder().configure().build();
+      private final SessionFactory sf
+       = new MetadataSources(registry).buildMetadata().buildSessionFactory();
+
+      @Override
+      public void close() {
+         StandardServiceRegistryBuilder.destroy(this.registry);
+      }
+   }
+}
+
+
